350fde14f862a6fecc68c973d1e1776ea7fe9a7e
Konstantinos Kalogeropoulos <kkalo878@gmail.com>
2022-06-07 16:50:51 +0300

    Adds SRP Demo | See details
    + A console application for demonstrating single responsibility principles.

---
+++ Final/SRP/SRP_Demo/SRP_Demo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRP_Demo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            StandardMessages.WelcomeMessage();
+
+            User user = UserDataCapture.Capture();
+
+            bool isUserValid = UserValidator.Validate(user);
+
+            if (isUserValid == false)
+            {
+                StandardMessages.EndApplication();
+                return;
+            }
+
+            RandomIDGenerator.createID(user);
+
+            StandardMessages.EndApplication();
+        }
+    }
+ 
+}
---
+++ Final/SRP/SRP_Demo/SRP_Demo/RandomIDGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRP_Demo
+{
+    public class RandomIDGenerator
+    {
+        public static void createID(User user)
+        {
+
+            //create an random ID for a user
+            string ID = Guid.NewGuid().ToString();
+            Console.WriteLine("Random ID for " + user.FirstName +" " + user.LastName + " is " + ID);
+
+        }
+    }
+}
