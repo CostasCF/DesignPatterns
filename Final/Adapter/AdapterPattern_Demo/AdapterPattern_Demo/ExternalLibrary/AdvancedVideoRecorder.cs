108f131264f6d8ff6c25f073bfc03401da310591
Konstantinos Kalogeropoulos <kkalo878@gmail.com>
2022-06-23 16:31:21 +0300

    Adds & Completes Adapter Pattern
    +Presentation for adapter pattern
    +Demo app for adapter pattern

---
+++ Final/Adapter/AdapterPattern_Demo/AdapterPattern_Demo/ExternalLibrary/AdvancedVideoRecorder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern_Demo
+{
+    public class AdvancedVideoRecorder : IAdvancedVideoRecorder
+    {
+        public void recordHighResolution(string screenID, string quality)
+        {
+            Console.WriteLine($"Recording at 4k on screen id {screenID}");
+        }
+    }
+}
---
+++ Final/Adapter/AdapterPattern_Demo/AdapterPattern_Demo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern_Demo
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            VideoRecorder recorder = new VideoRecorder();
+
+            recorder.record("1","1080p");
+            recorder.record("2", "4k");
+            Console.ReadLine();
+        }
+    }
+}
